9e7441496d5149eb9640f8b74a400a2388506c3f
ceci6182 <ceci6182@easv365.dk>
2022-10-26 11:05:30 +0200

    cors

--- moonbaboon.bingo.DataAccess/Repositories/UserRepository.cs
+++ moonbaboon.bingo.DataAccess/Repositories/UserRepository.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 using moonbaboon.bingo.Core.Models;
